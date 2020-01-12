cb292672e60a3b43557a0dd517aa320ca87f7b90
Andrew Capp <acapp909@gmail.com>
2020-01-12 22:58:53 +0000

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,36 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+
+  attr_reader :text
+
+  def initialize(text)
+    @text = text.downcase
+
+  end
+
+  def count_of_words
+    text.split(/ /).count
+  end
+
+  def count_of_vowels
+    text.scan(/[aeiou]/).count
+  end
+
+  def count_of_consonants
+    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def max_letters
+    result = text.scan(/\w/).reduce(Hash.new(0)) {|h, c| h[c] += 1; h}
+    sorted_result = result.sort_by{|key, value| value}.reverse
+    top = sorted_result[0][1]
+    max_letter = sorted_result.take_while{|key, value| value == top}
+  end
+
+  def min_letters
+    result = text.scan(/\w/).reduce(Hash.new(0)) {|h, c| h[c] += 1; h}
+    sorted_result = result.sort_by{|key, value| value}
+    bottom = sorted_result[0][1]
+    min_letters_result = sorted_result.take_while{|key, value| value == bottom}
+  end
+end
